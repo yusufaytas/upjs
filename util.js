303e8ac646c06905f815b6a4530058f02e91617c
Yusuf Aytas <yusufaytas@gmail.com>
2012-02-29 01:46:56 +0200

    UPJS 0.2.2

--- util.js
+++ util.js
@@ -59,6 +59,60 @@ _.util = (function(){
 			arr.push(item);
 	};
 	
+	// @arr - JavaScript array
+	// Returns a shuffled copy of the @array
+	u.shuffle = function(arr){
+		for(var i=0;i<arr.length;i++){
+			var i = (Math.random()*arr.length)|0;
+			var j = (Math.random()*arr.length)|0;
+			var temp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = temp;
+		}
+		return arr;
+	};
+	// @arr1,@arr2,...@arrn - Javascript Array
+	// Returns an union of the given arrays
+	u.union = function(){
+		var arr = [];
+		for(var i=0;i<arguments.length;i++)
+			arr = arr.concat(arguments[i]);
+		return u.uniquify(arr);
+	};
+	// @arr1,@arr2,...@arrn - Javascript Array
+	// Returns intersection of the given arrays
+	u.intersect = function(){
+		var arr = inter = [];
+		for(var i=0;i<arguments.length;i++)
+			arr = arr.concat(arguments[i]);
+		arr = u.uniquify(arr);
+		var count = [];
+		for(var i=0;i<arr.length;i++)
+			count.push(0);
+		for(var i=0;i<arr.length;i++){
+			for(var j=0;j<arguments.length;j++)
+				if(u.indexOf(arguments[j],arr[i])!=-1)
+					count[i]++;
+		}
+		for(var i=0;i<arr.length;i++)
+			if(count[i]==arguments.length)
+				inter.push(arr[i]);
+		return inter;
+	};
+	// @arr - JavaScript Array that is the reference point.
+	// @arr1,@arr2,...@arrn - Javascript Array
+	// Returns difference of @arr from @arr1 to @arrn
+	u.differ = function(){
+		var dif = arguments[0];
+		for(var i=1;i<arguments.length;i++){
+			var temp = [];
+			for(var j=0;j<dif.length;j++)
+				if(u.indexOf(arguments[i],dif[j])==-1)
+					temp.push(dif[j]);
+			dif = temp;
+		}
+		return dif;
+	};	
 	// @str - string to be searched
 	// @s - string to be found
 	// returns true or false if @str ends with @s
@@ -85,6 +139,10 @@ _.util = (function(){
 		copy : u.copy,
 		indexOf:u.indexOf,
 		pushIfNotExist:u.pushIfNotExist,
+		shuffle:u.shuffle,
+		union:u.union,
+		intersect:u.intersect,
+		differ:u.differ,
 		endsWith:u.endsWith,
 		startsWith:u.startsWith,
 		trim : u.trim
--- validator.js
+++ validator.js
@@ -8,8 +8,8 @@ _.validator = (function(){
 	function v(){};
 	// @value
 	// checks the given value is integer or not
-	v.isInt = function(value){
-		return v.patterns.intt.test(value);
+	v.isNumeric = function(value){
+		return v.patterns.numeric.test(value);
 	};
 	// @value
 	// checks the given value is alpha or not
@@ -36,35 +36,73 @@ _.validator = (function(){
 	v.isUrl = function(value){
 		return v.patterns.url.test(value);
 	};
+	
+	// @value
+	// checks the given value is phone number or not
+	v.isPhone = function(value){
+		return v.patterns.phone.test(value);
+	};
+	
+	// @value
+	// checks the given value is null or not
+	v.isNull = function(value){
+		return value==null;
+	};
+	
+	// @value
+	// checks the given value is undefined or not
+	v.isUndefined = function(value){
+		return value==undefined;
+	};
+	
+	// @value
+	// checks the given value is empty or not
+	v.isEmpty = function(value){
+		return value==""&&!value;
+	};
+	
+	// @value
+	// checks the given value is object or not
+	v.isObject = function(value){
+		return typeof(value) == "object";
+	};
+	
+	// @value
+	// checks the given value is function or not
+	v.isFunction = function(value){
+		return typeof(value) == "function";
+	};
+	
+	// @value
+	// checks the given value is HTML Element or not
+	v.isHTMLElement = function(value){
+		return !(!(value.tagName));
+	};
 	// patterns that are used for type checking
 	v.patterns = {
-		intt : /[-+]?[0-9]+/,
-		alpha : /[A-Za-z]+/,
+		numeric : /^[-+]?\d*[\.\,]?\d*$/,
+		alpha : /^[A-Za-z]+$/,
 		email : /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
-		date : /(\d{1,2}[\/.-]\d{1,2}[\/.-]\d{4})|(\d{4}[\/.-]\d{1,2}[\/.-]\d{1,2})/,
+		date : /^(\d{1,2}[\/.-]\d{1,2}[\/.-]\d{4})|(\d{4}[\/.-]\d{1,2}[\/.-]\d{1,2})^$/,
 		image: /(.)*\.(jpg|png|gif|bmp)$/,
-		url : /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/
+		url : /^(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/,
+		phone : /^\+?((\d *)|(\d-)){7,15}$/
 	};
 	
 	_.addExtension({
-		isInt:function(value){
-			return v.isInt(value);
-		},
-		isAlpha : function(value){
-			return v.isAlpha(value);
-		},
-		isEmail : function(value){
-			return v.isEmail(value);
-		},
-		isDate:function (value){
-			return v.isDate(value);
-		},
-		isImage:function (value){
-			return v.isImage(value);
-		},
-		isUrl:function (value){
-			return v.isUrl(value);
-		}
+		isNumeric:v.isNumeric,
+		isAlpha : v.isAlpha,
+		isEmail : v.isEmail,
+		isDate:v.isDate,
+		isImage:v.isImage,
+		isUrl:v.isUrl,
+		isPhone:v.isPhone,
+		isNull:v.isNull,
+		isUndefined:v.isUndefined,
+		isEmpty:v.isEmpty,
+		isObject:v.isObject,
+		isFunction:v.isFunction,
+		isHTMLElement:v.isHTMLElement
 	},true);
 	
 	return v;
